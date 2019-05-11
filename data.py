30ba60ecf2c6ca2f72f842438a0c179c84f2a406
Qinxue Yao <qinxue.yao@gmail.com>
2019-05-11 10:01:14 -0400

    add all

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title':'Article One',
+			'body': 'lorem	',
+			'author': 'Brad',
+			'create_date':'04-25-2017'
+		},
+		{
+			'id': 2,
+			'title':'Article Two',
+			'body': 'loremdddd	',
+			'author': 'John',
+			'create_date':'04-25-2017'
+		},
+		{
+			'id': 3,
+			'title':'Article Three',
+			'body': 'loremdddd	',
+			'author': 'John',
+			'create_date':'04-25-2017'
+		}
+	]
+	return articles
